0e7d73d9a3a323bbe49f4d1e5499c71754456f12
Guillaume Clement <gclement@baobob.org>
2014-02-17 00:07:00 +0100

    Externalize the call of imap in order to prepare its inclusion in a backround thread.

--- qimaptray.cpp
+++ qimaptray.cpp
@@ -20,29 +20,18 @@ static void logout(int)
 	}
 }
 
-int main(int argc, char **argv)
+
+void wait_messages(const char *host, const char *login, const char *password, imappp::imap::receive_message_callback callback)
 {
-	if (argc < 4)
-	{
-		fprintf(stderr, "usage : %s server user pass\n", argv[0]);
-		return 1;
-	}
-
-	signal(SIGINT, logout);
-
 	try
 	{
-		imappp::imap i(argv[1], true);
+		imappp::imap i(host, true);
 
-		i.set_message_callback(
-			[](unsigned int count)
-			{
-				printf("Currently we have %u unread messages.\n", count);
-			});
+		i.set_message_callback(callback);
 
 		conn = &i;
 
-		if (i.login(argv[2], argv[3]))
+		if (i.login(login, password))
 		{
 			i.select("INBOX");
 
@@ -60,5 +49,23 @@ int main(int argc, char **argv)
 	{
 		fprintf(stderr, "Error: %s\n", str);
 	}
+}
+
+int main(int argc, char **argv)
+{
+	if (argc < 4)
+	{
+		fprintf(stderr, "usage : %s server user pass\n", argv[0]);
+		return 1;
+	}
+
+	signal(SIGINT, logout);
+
+	wait_messages(argv[1], argv[2], argv[3],
+		[](unsigned int count)
+		{
+			printf("Currently we have %u unread messages.\n", count);
+		});
+
 	return 0;
 }
