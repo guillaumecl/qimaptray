af503b70e4c7d5d201fcec61fe95cbbba3858a4e
Guillaume Clement <gclement@baobob.org>
2014-02-28 09:46:17 +0100

    Show a message box on connection issues if not run in terminal, fixes #5

--- qimaptray.cpp
+++ qimaptray.cpp
@@ -1,55 +1,70 @@
 #include <stdio.h>
+#include <unistd.h>
 
 #include "connection.h"
 #include "tray.h"
 
 #include <QApplication>
 #include <QThread>
+#include <QMessageBox>
 
 int main(int argc, char **argv)
 {
 	QApplication app(argc, argv);
-	char *host;
-	char *user;
-	char *password;
-
-	if (argc == 1)
+	try
 	{
-		host = (char*)alloca(256);
-		user = (char*)alloca(256);
-		password = (char*)alloca(256);
-		fscanf(stdin, "%255s %255s %255s", host, user, password);
+		char *host;
+		char *user;
+		char *password;
+
+		if (argc == 1)
+		{
+			host = (char*)alloca(256);
+			user = (char*)alloca(256);
+			password = (char*)alloca(256);
+			fscanf(stdin, "%255s %255s %255s", host, user, password);
+		}
+		else if (argc < 4)
+		{
+			fprintf(stderr, "usage : %s server user pass\n", argv[0]);
+			return 1;
+		}
+		else
+		{
+			host = argv[1];
+			user = argv[2];
+			password = argv[3];
+		}
+
+		QThread thread;
+
+		tray t;
+		connection c(host, user, password);
+
+		// don't keep the password stored in RAM
+		memset(password, 0, strlen(password));
+
+		c.moveToThread(&thread);
+		QObject::connect(&c, SIGNAL(new_unread_count(unsigned int, unsigned int)),
+						 &t, SLOT(unread_messages(unsigned int, unsigned int)));
+
+		thread.start();
+		return app.exec();
 	}
-	else if (argc < 4)
+	catch(const std::exception&e)
 	{
-		fprintf(stderr, "usage : %s server user pass\n", argv[0]);
-		return 1;
+		if (not isatty(fileno(stdin)))
+		{
+			QMessageBox::critical(nullptr, "qimaptray", QString::fromStdString(e.what()));
+		}
 	}
-	else
+	catch(const char *e)
 	{
-		host = argv[1];
-		user = argv[2];
-		password = argv[3];
+		if (not isatty(fileno(stdin)))
+		{
+			QMessageBox::critical(nullptr, "qimaptray", e);
+		}
 	}
 
-	QThread thread;
-
-	tray t;
-	connection c(host, user, password);
-
-	// don't keep the password stored in RAM
-	memset(password, 0, strlen(password));
-
-	c.moveToThread(&thread);
-	QObject::connect(&c, SIGNAL(new_unread_count(unsigned int, unsigned int)),
-					 &t, SLOT(unread_messages(unsigned int, unsigned int)));
-
-	thread.start();
-
-	int r = app.exec();
-
-	// thread.quit();
-	// thread.wait();
-
-	return r;
+	return 1;
 }
