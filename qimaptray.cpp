48117dddb68c5a0ab973564d4b890d6350372139
Guillaume Clement <gclement@baobob.org>
2014-02-28 09:35:53 +0100

    Allow to read user input from stdin

--- qimaptray.cpp
+++ qimaptray.cpp
@@ -9,17 +9,36 @@
 int main(int argc, char **argv)
 {
 	QApplication app(argc, argv);
+	char *host;
+	char *user;
+	char *password;
 
-	if (argc < 4)
+	if (argc == 1)
+	{
+		host = (char*)alloca(256);
+		user = (char*)alloca(256);
+		password = (char*)alloca(256);
+		fscanf(stdin, "%255s %255s %255s", host, user, password);
+	}
+	else if (argc < 4)
 	{
 		fprintf(stderr, "usage : %s server user pass\n", argv[0]);
 		return 1;
+	}
+	else
+	{
+		host = argv[1];
+		user = argv[2];
+		password = argv[3];
 	}
 
 	QThread thread;
 
 	tray t;
-	connection c(argv[1], argv[2], argv[3]);
+	connection c(host, user, password);
+
+	// don't keep the password stored in RAM
+	memset(password, 0, strlen(password));
 
 	c.moveToThread(&thread);
 	QObject::connect(&c, SIGNAL(new_unread_count(unsigned int, unsigned int)),
