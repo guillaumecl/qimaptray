088d6557c31c7d2694b827e615e78934f92ddbec
Guillaume Clement <gclement@baobob.org>
2014-08-03 13:01:06 +0200

    webcam_led: Webcam mode is now optional.
    Using webcam_led adds the v4l2 dependency. It's better to have it
    optional to allow having lighter dependencies if needed.
    
    Signed-off-by: Guillaume Clement <gclement@baobob.org>

---
+++ webcam_dummy.cpp
@@ -0,0 +1,39 @@
+/* Based on:
+   V4L2 video picture grabber
+   Copyright (C) 2009 Mauro Carvalho Chehab <mchehab@infradead.org>
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation version 2 of the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+*/
+#include "webcam.h"
+
+namespace qimaptray
+{
+
+webcam::webcam()
+{
+}
+
+webcam::~webcam()
+{
+}
+
+void webcam::set_light(bool)
+{
+}
+
+void webcam::light()
+{
+}
+
+void webcam::unlight()
+{
+}
+
+}
